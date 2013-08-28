57b5f8aa7e0d9b1b4cd9515cb8b4dcfc66468334
vmayoral <v.mayoralv@gmail.com>
2013-08-28 18:57:01 +0200

    recipes folder renamed

--- scripts/minimal-ros-install-angstrom.sh
+++ scripts/minimal-ros-install-angstrom.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "updating the Angstrom feed to eu.feeds.angstrom-distribution..."
-echo "src/gz base http://eu.feeds.angstrom-distribution.org/feeds/v2012.12/ipk/eglibc/armv7a-vfp-neon/base" > /etc/opkg/base-feed.conf
-echo "updating the python feed to eu.feeds..."
-echo "src/gz python http://eu.feeds.angstrom-distribution.org/feeds/v2012.12/ipk/eglibc/armv7a-vfp-neon/python" > /etc/opkg/python-feed.conf
+#echo "updating the Angstrom feed to eu.feeds.angstrom-distribution..."
+#echo "src/gz base http://eu.feeds.angstrom-distribution.org/feeds/v2012.12/ipk/eglibc/armv7a-vfp-neon/base" > /etc/opkg/base-feed.conf
+#echo "updating the python feed to eu.feeds..."
+#echo "src/gz python http://eu.feeds.angstrom-distribution.org/feeds/v2012.12/ipk/eglibc/armv7a-vfp-neon/python" > /etc/opkg/python-feed.conf
 
 # update the feeds
 opkg update
